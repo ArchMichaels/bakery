608e8951c09107e3229b500a9fde0e1009d59a16
ArchMichaels <michaelobilana@gmail.com>
2026-06-08 22:47:34 +0100

    update

--- papasbakery/papasbakeryapp/models.py
+++ papasbakery/papasbakeryapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=500)
+    quest_date = models.DateTimeField("date the question was asked")
+
+
+class Allergies(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    #votes = models.IntegerField(default=0)
